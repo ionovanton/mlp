b0ca611459ede57cc4146d7df790878478b559c1
kofesmolotkom <manuvaad@gmail.com>
2023-01-22 21:28:37 +0300

    empty

--- src/Matrix/main.cpp
+++ src/Matrix/main.cpp
@@ -1,8 +1,6 @@
 
 #include "header.hpp"
 
-
-
 template<typename T>
 struct aligned_ptr {
 	using pointer = T*;
@@ -13,17 +11,18 @@ struct aligned_ptr {
 	size_type size;
 	aligned_ptr(const_size_type size = 0) : 
 	m_field{new (std::align_val_t{ alignof(T) }) T[size]}, size{size} { class_func_log; }	// aligned allocation
-	// m_field{new T[size]}, size{size} { class_func_log; }										// standard allocation
+	// m_field{new T[size]}, size{size} { class_func_log; }									// standard allocation
 
 	/* deep copy */
-	aligned_ptr(const aligned_ptr &other) :
+	aligned_ptr(const aligned_ptr<T> &other) :
 	m_field{new (std::align_val_t{ alignof(T) }) T[other.size]}, size{other.size} {
 		class_func_log;
 		for (std::size_t i = 0; i < size; ++i)
 			m_field[i] = other.m_field[i];
 	};
-	aligned_ptr &operator=(const aligned_ptr &other) {
+	aligned_ptr<T> &operator=(const aligned_ptr<T> &other) {
 		class_func_log;
+
 		delete[] m_field;
 		m_field = new (std::align_val_t{ alignof(T) }) T[other.size];
 		size = other.size;
@@ -50,15 +49,34 @@ struct aligned_ptr {
 		other.m_field = nullptr;
 		return *this;
 	}
-	~aligned_ptr() { class_func_log; delete[] m_field; }
+	~aligned_ptr() { delete[] m_field; class_func_log; }
 
 	__attribute__((always_inline))
 	void advance(size_type n) { m_field += n; }
 	__attribute__((always_inline))
 	void unwind(size_type n) { m_field -= n; }
-	__attribute__((always_inline))
-	pointer operator*() { return m_field; }
+	// __attribute__((always_inline))
+	// pointer operator*() { return m_field; }
 };
+
+template<typename C>
+void inspect_container(C &c) {
+	std::cout << "vector: " << &c << "\n";
+	std::cout << "	size: " << c.size() << "\n";
+	std::cout << "	cap: " << c.capacity() << "\n";
+	for (auto &e : c) {
+		std::cout << "	" << &e << " " << e << "\n";
+	}
+	std::cout << std::endl;
+}
+
+template<typename T>
+void inspect_ptr(aligned_ptr<T> &p) {
+	std::cout << "aligned_ptr: " << &p << "\n";
+	for (auto i = p.size * 0; i < p.size; ++i)
+		std::cout << "	" << &p.m_field[i] << " " << p.m_field[i] << "\n";
+	std::cout << std::endl;
+}
 
 
 template <class T, std::size_t N>
@@ -101,7 +119,7 @@ struct linear_alloc {
 		class_func_log;
 		auto ret_ptr = m_current;
 		m_current += n;
-		++m_num_allocations;
+		m_num_allocations += n % m_size;
 		return ret_ptr;
 	}
 
@@ -111,39 +129,17 @@ private:
 	const aligned_ptr<T> m_start { N };
 	pointer m_current { m_start.m_field };
 	size_type m_num_allocations { 0 };
-	// size_type m_size { N };
+	size_type m_size { N };
 	// size_type m_used_bytes { 0 };
 };
-
-template<typename C>
-void inspect_container(C &c) {
-	std::cout << "vector: " << &c << "\n";
-	std::cout << "	size: " << c.size() << "\n";
-	std::cout << "	cap: " << c.capacity() << "\n";
-	for (auto &e : c) {
-		std::cout << "	" << &e << " " << e << "\n";
-	}
-	std::cout << std::endl;
-}
-
-template<typename T>
-void inspect_ptr(aligned_ptr<T> &p) {
-	std::cout << "aligned_ptr: " << &p << "\n";
-	for (auto i = p.size * 0; i < p.size; ++i)
-		std::cout << "	" << &p.m_field[i] << " " << p.m_field[i] << "\n";
-	std::cout << std::endl;
-}
 
 using namespace std;
 
 int main() {
-	const int n = 1;
+	const int n = 20000;
 	using value_type = int;
 	using alloc = linear_alloc<value_type, n>;
 
 	std::vector<value_type, alloc> a(n);
-
-	// aligned_ptr<int> x(20);
-
-	// inspect_container(a);
+	// a[0] = 21;
 }
