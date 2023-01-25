44cf1b94c36c9b028b8853be33997816b1d62dd0
kofesmolotkom <manuvaad@gmail.com>
2023-01-26 01:24:31 +0300

    custom allocation done

--- src/Matrix/main.cpp
+++ src/Matrix/main.cpp
@@ -1,4 +1,3 @@
-
 #include "header.hpp"
 
 template<typename C>
@@ -12,17 +11,56 @@ void inspect_container(C &c) {
 	std::cout << std::endl;
 }
 
-template <class T, std::size_t N>
-struct linear_alloc {
+template<typename>
+class linear_alloc;
+
+template<typename T>
+class allocator_state {
+	friend class linear_alloc<T>;
 	using value_type = T;
 	using size_type = size_t;
 	using const_size_type = const size_t;
 	using pointer = T*;
 
-	template<typename U>
-	struct rebind {
-		using other = linear_alloc<U, N>;
-	};
+	static std::shared_ptr<value_type[]> m_start;
+	static pointer m_current;
+	static size_type m_num_allocations;
+	static size_type m_size;
+
+public:
+    allocator_state() = delete;
+    allocator_state(const allocator_state&) = delete;
+    allocator_state &operator=(const allocator_state&) = delete;
+    allocator_state(const allocator_state&&) = delete;
+    allocator_state &operator=(const allocator_state&&) = delete;
+
+    static void init(const_size_type n) {
+        allocator_state<value_type>::m_start = static_cast<std::shared_ptr<value_type[]>>
+        (new (std::align_val_t{ alignof(value_type) }, std::nothrow) value_type[n]);
+        allocator_state<value_type>::m_current = allocator_state<value_type>::m_start.get();
+        allocator_state<value_type>::m_num_allocations = 0;
+        allocator_state<value_type>::m_size = n;
+    }
+};
+
+template<typename T>
+std::shared_ptr<T[]> allocator_state<T>::m_start;
+
+template<typename T>
+T* allocator_state<T>::m_current;
+
+template<typename T>
+std::size_t allocator_state<T>::m_num_allocations;
+
+template<typename T>
+std::size_t allocator_state<T>::m_size;
+
+template<typename T>
+struct linear_alloc {
+	using value_type = T;
+	using size_type = size_t;
+	using const_size_type = const size_t;
+	using pointer = T*;
 
 	linear_alloc(const linear_alloc&) noexcept = default;
 	linear_alloc() noexcept = default;
@@ -30,44 +68,54 @@ struct linear_alloc {
 	
 	/* what is this for? */
 	template <class U>
-	linear_alloc(const linear_alloc<U, N>&) noexcept {}
+	linear_alloc(const linear_alloc<U>&) noexcept {}
 
 	[[nodiscard]] pointer allocate(size_type n) noexcept {
-		auto ret_ptr = m_current;
-		m_current += n;
-		m_num_allocations += n;
+		auto ret_ptr = allocator_state<T>::m_current;
+		allocator_state<T>::m_current += n;
+		allocator_state<T>::m_num_allocations += n;
 		return ret_ptr;
 	}
 
 	void deallocate([[maybe_unused]] pointer, [[maybe_unused]] size_type) {}
+};
 
-private:
-	std::shared_ptr<T[]> m_start{ new (std::align_val_t{ alignof(T) }) T[N] };
-	pointer m_current { m_start.get() };
-	size_type m_num_allocations { 0 };
-	size_type m_size { N };
+template<class T, class U>
+bool operator==(const linear_alloc<T>&, const linear_alloc<U>&) { return true; }
+ 
+template<class T, class U>
+bool operator!=(const linear_alloc<T>&, const linear_alloc<U>&) { return false; }
+
+
+template<typename T, std::size_t N>
+struct test {
+    test() : a(N / 2), b(N / 2) {};
+    std::vector<T, linear_alloc<T>> a;
+    std::vector<T, linear_alloc<T>> b;
 };
 
 using namespace std;
 
-template<typename T, std::size_t N, typename Alloc>
-struct test {
-
-	std::size_t n { N };
-	std::vector<T, linear_alloc<T, N>> a { std::vector<T, linear_alloc<T, N>>(N) };
-	std::vector<T, linear_alloc<T, N>> b { std::vector<T, linear_alloc<T, N>>(N) };
-};
-
 int main() {
-	const int n = 2;
-	// linear_alloc<int, n> x;
-
-	// std::vector<int, linear_alloc<int, n>> a(n);
-	// a[0] = 21;
-	// a[1] = 21;
-	// a[2] = 21;
-
-	test<int, 10, linear_alloc<int, 10>> x;
+    using program_type = float;
+    const size_t n = 6;
+    allocator_state<program_type>::init(n);
+    test<program_type, n> t;
 
 
+	t.a[0] = 41;
+	t.a[1] = 23;
+	t.a[2] = 890;
+	t.b[0] = 99;
+	t.b[1] = 1;
+	t.b[2] = 33;
+
+	inspect_container(t.a);
+	inspect_container(t.b);
+
+	auto ptr = &t.a[0];
+	cout << *ptr << endl;
+
+	for (int i = 0; i < n; ++i, ++ptr)
+		cout << ptr << ' ' << *ptr << '\n';
 }
